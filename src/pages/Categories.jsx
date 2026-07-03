5e12f43ff33d9c7972dd2d04be1f5a169d955c17
Sanikajagtap05 <sanikajagatap164@gmail.com>
2026-07-03 17:27:17 +0530

    Updated categories page layout and category card styling

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -3,34 +3,31 @@ import CategoryCard from "../pages/CategoryCard";
 
 function Categories() {
   return (
-    <div className="container mt-4">
+    <div className="container-fluid mt-3">
       <h2
         style={{
-          marginBottom: "20px",
+          marginBottom: "15px",
+          fontSize: "22px",
           fontWeight: "500",
-          fontSize: "32px",
         }}
       >
         Categories
       </h2>
 
-      <div className="row g-4">
+      <div className="row g-3">
         {categories.map((category) => (
           <div
             key={category.slug}
-            className="col-xl-3 col-lg-4 col-md-6 col-sm-6"
+            className="col-xl-2 col-lg-2 col-md-4 col-sm-6"
           >
             <div
               style={{
-                backgroundColor: "#f5f5f5",
-                padding: "22px",
-                height: "110px",
+                background: "#f5f5f5",
+                height: "105px",
+                padding: "20px",
                 display: "flex",
                 flexDirection: "column",
                 justifyContent: "center",
-                borderRadius: "2px",
-                transition: "0.3s",
-                cursor: "pointer",
               }}
             >
               <CategoryCard
--- src/pages/CategoryCard.jsx
+++ src/pages/CategoryCard.jsx
@@ -2,31 +2,39 @@ import { Link } from "react-router-dom";
 
 function CategoryCard({ name, slug }) {
   return (
-    <>
-      <h4
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
+      <h5
         style={{
-          fontSize: "18px",
-          fontWeight: "500",
-          marginBottom: "12px",
+          fontSize: "17px",
+          fontWeight: "400",
           color: "#000",
+          margin: "0 0 12px 0",
         }}
       >
         {name}
-      </h4>
+      </h5>
 
       <Link
         to={`/category/${slug}`}
         style={{
           color: "#3f6fff",
           textDecoration: "none",
-          fontSize: "15px",
-          textTransform: "uppercase",
+          fontSize: "14px",
           fontWeight: "500",
+          textTransform: "uppercase",
+          letterSpacing: "0.3px",
         }}
       >
         VIEW PRODUCTS
       </Link>
-    </>
+    </div>
   );
 }
 
